cfff88abee6a76a302ac7d434e946fb13a1ddb35
pouria2003 <s.pouria.h2003@gmail.com>
2024-05-26 20:02:22 +0430

    changed database.py

--- Src/databaseConnection.py
+++ Src/databaseConnection.py
@@ -1,2 +1,12 @@
-f = open("..\database.txt", "a+")
-f.write("username passw[/[/ord\n")
+
+import serial
+
+# Connection propeties
+port = 'COM15'
+bound_rate = 9600
+
+ser_con = serial.Serial(port, bound_rate)
+
+while(1) {
+    bytes data_from_port = ser_con.read_until()
+}
